5b5606bfbe4370c83e2abcde5d0174cbf6df90c5
tmarcorf <tmarco783@gmail.com>
2026-07-30 12:30:11 -0300

    feat: add analytics

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,8 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
